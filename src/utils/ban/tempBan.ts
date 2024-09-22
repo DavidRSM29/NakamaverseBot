5911667e1b5b1f60d04e2ff5cf3417db1513ab33
David S. <40255474+DavidRSM29@users.noreply.github.com>
2024-09-22 01:55:03 +0000

    Removed return statement since it is not needed

--- src/utils/ban/tempBan.ts
+++ src/utils/ban/tempBan.ts
@@ -1,5 +1,5 @@
-import { CacheType, ChatInputCommandInteraction } from "discord.js";
-import { BanDate } from "../date";
+import { CacheType, ChatInputCommandInteraction, time } from "discord.js";
+import { BanDate } from "../banDate";
 import { Ban } from "./ban";
 
 export class TempBan extends Ban {
@@ -8,9 +8,9 @@ export class TempBan extends Ban {
         try {
             const targetUser = await this.isBanValid(interaction)
             const timeStamp = await new BanDate(interaction.createdAt).addTime(interaction.options.getString('tiempo', true), interaction.createdAt);
-            return await interaction.editReply({ content: `Usuario temporalmente vetado exitosamente, El veto termina ${timeStamp}` })
+            await interaction.editReply({ content: `Usuario temporalmente vetado exitosamente, El veto termina ${timeStamp}` })
         } catch (error) {
-            if (error instanceof Error) return await interaction.editReply({ content: `${error.name}: ${error.message}` })
+            if (error instanceof Error) await interaction.editReply({ content: `${error.name}: ${error.message}` })
         }
     }
 }
