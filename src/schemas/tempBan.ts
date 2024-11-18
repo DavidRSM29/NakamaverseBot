420abb58cfb254acafdc5932ca31b426463ba416
DavidRSM29 <davidotno@gmail.com>
2024-11-17 22:27:23 -0600

    removing old punish command files

--- src/schemas/tempBan.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const tempBanSchema = new Schema(
-    {
-        _id: { type: String, required: true },
-    }
-)
-
-export default model('TempBan', tempBanSchema)
